f4b940fd72650e20062dc863233a615d7ddd6c6c
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-07-12 11:55:59 +0100

    Dynamic Prompt builder

--- src/input/instruction.rs
+++ src/input/instruction.rs
@@ -22,3 +22,20 @@ pub enum InstructionType {
     /// TODO: Output will most likely be a beast in it of itself..
     OutputSchema,
 }
+
+impl Instruction {
+    pub fn to_string(&self) -> String {
+        let prelude = match self.instruction_type {
+            InstructionType::Purpose => "\n- Purpose:",
+            InstructionType::Audience => "\n- Audience:",
+            InstructionType::Complexity => "\n- Complexity:",
+            InstructionType::Context => "\n- Context:",
+            InstructionType::Restriction => "\n- Restriction:",
+            InstructionType::Principle => "\n- Principle:",
+            InstructionType::ResponseScope => "\n- Response scope (open-ended vs. close-ended):",
+            InstructionType::OutputSchema => "\n- Output schema/format:",
+        };
+
+        format!("{} {}", prelude, self.inner)
+    }
+}
--- src/input/prompt_builder.rs
+++ src/input/prompt_builder.rs
@@ -1,26 +1,36 @@
+use super::instruction::Instruction;
 use crate::ai::openai::{
     client::OpenAI,
     input::{GptRole, Message},
 };
+use anyhow::Result;
 
-use super::instruction::Instruction;
-
-pub fn build_prompt_dyn<'a>(
-    client: &'a OpenAI,
-    instruction: &'a [Instruction],
-    sys_msg: Option<&'a str>,
-    user_msg: Option<&'a str>,
-) -> &'a str {
+pub async fn build_prompt_dyn(
+    client: &OpenAI,
+    instructions: &[Instruction],
+    sys_msg: Option<&str>,
+    user_msg: Option<&str>,
+) -> Result<String> {
     let sys_msg = Message {
         role: GptRole::System,
         content: String::from(sys_msg.unwrap_or("You are Prompt Generator specialised in receiving a list of instructions and outputing prompts that maximise the effectiveness of the prompt output.")),
     };
 
-    let user_msg = user_msg.unwrap_or("Build a prompt which follows the instructions below: \n");
+    let mut user_msg =
+        String::from(user_msg.unwrap_or("Build a prompt which follows the instructions below: \n"));
 
-    // let resp = client.chat(&[sys_msg, user_msg], &[], &[]).await?;
+    for instruction in instructions.iter() {
+        user_msg.push_str(&instruction.to_string());
+    }
 
-    // resp.choices.first().unwrap().message.content.as_str()
+    let user_msg = Message {
+        role: GptRole::User,
+        content: user_msg,
+    };
 
-    todo!()
+    let resp = client.chat(&[sys_msg, user_msg], &[], &[]).await?;
+
+    let prompt = resp.choices.first().unwrap().message.content.as_str();
+
+    Ok(prompt.to_string())
 }
