af3e4e677c7d132ed9edf2bbcc8313a862f12f14
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-09-04 13:45:56 +0100

    Small fix

--- client/src/commands/startPrompt.ts
+++ client/src/commands/startPrompt.ts
@@ -16,7 +16,7 @@ export async function startPrompt(
 
     if (userInput !== undefined) {
       const taskType = wasm.TaskType.ScaffoldProject;
-      const taskPayload = new wasm.ScaffoldParams();
+      const taskPayload = new wasm.ScaffoldParams(userInput);
       const taskParams = new wasm.TaskParams(taskType, taskPayload);
 
       await appState.scaffoldProject(llmClient, llmParams, taskParams);
