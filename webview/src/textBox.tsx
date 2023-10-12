7c63d4904d875727195443cd9a9a2fbfa9cf18a1
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-10-12 19:02:16 +0100

    Foldeable input text box

--- webview/src/textBox.tsx
+++ webview/src/textBox.tsx
@@ -3,6 +3,7 @@ import SVGButton from './sendButton';
 
 const TextBox: React.FC<{ onSendMessage: (text: string) => void }> = ({ onSendMessage }) => {
   const [text, setText] = useState('');
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
   const handleSend = () => {
     if (text.trim()) {
@@ -11,16 +12,28 @@ const TextBox: React.FC<{ onSendMessage: (text: string) => void }> = ({ onSendMe
     }
   };
 
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = '2px';
+      const computed = window.getComputedStyle(textareaRef.current);
+      const height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+          + parseInt(computed.getPropertyValue('border-bottom-width'), 10)
+          + textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = `${height}px`;
+    }
+  }, [text]);
+
   return (
     <div className="textBox">
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Send a message"
-      />
-      <SVGButton onClick={handleSend} />
+        <textarea
+            ref={textareaRef}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Send a message"
+        />
+        <SVGButton onClick={handleSend} />
     </div>
-  );
+);
 };
 
 export default TextBox;
