2a7d359738f3eb7142046d13a11689766bc687d7
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-10-22 11:29:22 +0100

    Checkpoint: TextBox with rendered html

--- webview/src/ components/textBox.tsx
+++ webview/src/ components/textBox.tsx
@@ -3,45 +3,82 @@ import SVGButton from './sendButton';
 
 const TextBox: React.FC<{ onSendMessage: (text: string) => void }> = ({ onSendMessage }) => {
   const [text, setText] = useState('');
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [rawText, setRawText] = useState('');  // Store the original unformatted text
+  const textDivRef = useRef<HTMLDivElement | null>(null);
 
   const handleSend = () => {
-    if (text.trim()) {
-      onSendMessage(text.trim());
+    if (rawText.trim()) {
+      onSendMessage(rawText.trim());
       setText('');
+      setRawText('');  // Reset the original text too
     }
   };
 
   const handleKeyPress = (event: React.KeyboardEvent) => {
     if (event.key === 'Enter' && !event.shiftKey && !event.ctrlKey && !event.altKey) {
-      event.preventDefault();  // Prevents the default behavior of the Enter key (i.e., a newline)
+      event.preventDefault();
       handleSend();
     }
   };
 
-  useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = '2px';
-      const computed = window.getComputedStyle(textareaRef.current);
-      const height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-          + parseInt(computed.getPropertyValue('border-bottom-width'), 10)
-          + textareaRef.current.scrollHeight;
-      textareaRef.current.style.height = `${height}px`;
+  const handleChange = () => {
+    if (textDivRef.current) {
+      const currentPos = saveCaretPosition(textDivRef.current); // Save current position
+
+      const content = textDivRef.current.innerText;  // Get the plain text
+      setRawText(content);  // Update the original text
+
+      // Formatting for the display within the div
+      const formattedContent = content
+        .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')  // for bold
+        .replace(/`(.*?)`/g, '<span class="one-liner-code">$1</span>')  // for inline code
+        .replace(/```(.*?)```/g, '<div class="code-block">$1</div>');  // for code block
+
+      setText(formattedContent);
+      textDivRef.current.innerHTML = formattedContent;
+
+      restoreCaretPosition(textDivRef.current, currentPos); // Restore position
     }
-  }, [text]);
+  };
 
   return (
     <div className="textBox">
-        <textarea
-            ref={textareaRef}
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            onKeyPress={handleKeyPress}  // Add this line
+        <div
+            ref={textDivRef}
+            contentEditable={true}
+            onInput={handleChange}
+            onKeyDown={handleKeyPress}
             placeholder="Send a message"
-        />
+            className="editable-div"
+        ></div>
         <SVGButton onClick={handleSend} />
     </div>
-);
+  );
 };
+
+const saveCaretPosition = (context: HTMLElement): number => {
+  const selection = window.getSelection();
+  if (selection && selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    range.setStart(context, 0);
+    const len = range.toString().length;
+    return len;
+  }
+  return 0;
+}
+
+const restoreCaretPosition = (context: HTMLElement, pos: number): void => {
+  const selection = window.getSelection();
+  if (selection) {
+    const range = document.createRange();
+    if (context.firstChild) {
+      range.setStart(context.firstChild, pos);
+      range.collapse(true);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  }
+}
+
 
 export default TextBox;
