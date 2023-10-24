878e7866df3e555fd69532207777a63e9b4467da
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-10-24 13:06:26 +0100

    Icon style

--- webview/src/ components/reactQuill.tsx
+++ webview/src/ components/reactQuill.tsx
@@ -1,15 +1,27 @@
 // QuillEditor.tsx
 
 import React from 'react';
-import ReactQuill from 'react-quill';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';  // import styles
+import 'font-awesome/css/font-awesome.min.css';
+
+var icons = Quill.import('ui/icons');
+// icons['bold'] = '<i class="fa fa-bold" aria-hidden="true"></i>';
+// icons['italic'] = '<i class="fa fa-italic" aria-hidden="true"></i>';
+// icons['underline'] = '<i class="fa fa-underline" aria-hidden="true"></i>';
+// icons['image'] = '<i class="fa fa-picture-o" aria-hidden="true"></i>';
+icons['code'] = '<i class="fa fa-code" aria-hidden="true"></i>';
+icons['code-block'] = '<i class="fas fa-file-code-o" aria-hidden="true"></i>';
 
 const CustomToolbar: React.FC = () => {
     return (
         <div id="toolbar">
-            {/* Add other toolbar items as needed */}
-            <button className="ql-code">Code</button>
-            <button className="ql-code-block">Code Block</button>
+            <button className="ql-code">
+                {/* <i className="fa fa-code"></i> */}
+            </button>
+            <button className="ql-code-block">
+                {/* <i className="fa fa-file-code-o"></i> */}
+            </button>
         </div>
     );
 };
