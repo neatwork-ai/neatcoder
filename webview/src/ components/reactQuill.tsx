20d70b6e7f471051cdc394d33cf079e37ce2837d
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-10-26 13:49:13 +0100

    Start inline code with ctrl + shit + i

--- webview/src/ components/reactQuill.tsx
+++ webview/src/ components/reactQuill.tsx
@@ -5,11 +5,11 @@ import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';  // import styles
 import 'font-awesome/css/font-awesome.min.css';
 import deltaToMarkdown from '../quillToMarkdown/fromDelta';
+import { RangeStatic } from 'quill';
 
 const delayLoop = (iterations: number) => {
     for (let i = 0; i < iterations; i++) { }
 };
-
 
 var icons = Quill.import('ui/icons');
 // icons['bold'] = '<i class="fa fa-bold" aria-hidden="true"></i>';
@@ -62,70 +62,52 @@ export const QuillEditor: React.FC<{ onSendMessage: (text: string) => void }> = 
     useEffect(() => {
         if (quillRef.current) {
             const quill = quillRef.current.getEditor();
+            let consecutiveBackticks = 0;
 
             // Disable default behaviour of `Enter`
             const keyboard = quill.getModule('keyboard');
             keyboard.bindings['Enter'] = null;
             keyboard.bindings['13'] = null;
 
-            let consecutiveBackticks = 0;  // Track consecutive backtick presses
+            let consecutivePresses = 0;
+
+            keyboard.addBinding({
+                key: 'I',
+                shiftKey: true,
+                shortKey: true,
+            }, (range: RangeStatic, context: KeyboardEvent) => {
+                consecutivePresses += 1;
+
+                console.log('Command + Shift + I pressed');
+                quill.format('code', true);
+
+                if (consecutivePresses === 2) {
+                    quill.format('code-block', true);
+                }
+            });
+
+            console.log("keyboard.bindings: " + JSON.stringify(keyboard.bindings));
 
             const handleKeyDown = (event: KeyboardEvent) => {
                 if (event.key === 'Enter' && !event.shiftKey && !event.ctrlKey && !event.altKey) {
                     handleSend();
                 }
 
-                if (event.code === "BracketRight") {  // Key code for backtick
-                    consecutiveBackticks++;
-                    console.log("consecutiveBackticks: " + consecutiveBackticks);
+                if (event.key === 'Enter') {
+                    const selection = quill.getSelection();
+                    const format = quill.getFormat(selection!);
 
-                    if (consecutiveBackticks === 3) {
-                        console.log("Three backticks!");
-                        const currentSelection = quill.getSelection(); // Get current selection
-                        if (currentSelection) {
-                            console.log("There's a selection");
-                            const startIndex = Math.max(currentSelection.index - 3, 0); // Start from the first backtick
-                            quill.removeFormat(startIndex, 3); // Remove any existing format for the three backticks
-                            quill.deleteText(startIndex, 2);
-                            quill.formatText(startIndex, 3, 'code-block', true); // Apply the code block format
-
-                            const selectIndex = startIndex + 3; // this literally has to be calculated in separate, otherwise javascript concatenates them as strings lol
-                            quill.setSelection(selectIndex, 0); // Set the cursor after the third backtick
-                        }
-                        event.preventDefault(); // Prevent the third backtick from being typed into the editor
-                        consecutiveBackticks = 0; // Reset the counter
+                    if (format.code) {
+                        // If inside inline code, prevent the newline
+                        event.preventDefault();
+                        return;
                     }
+                }
 
-                } else {
-                    // Create inline code format
-                    if (consecutiveBackticks === 1) {
-                        const currentSelection = quill.getSelection();
-                        if (currentSelection) {
-                            const startIndex = Math.max(currentSelection.index - 1, 0); // Start from the backtick
-
-                            // We delay the application of the format by a few millisecond
-                            // to allow the browser to process the initial keystroke. We do this
-                            // because if not the browser adds another backtick which is undesirable
-
-                            const formatStartIndex = startIndex + 1; // this literally has to be calculated in separate, otherwise javascript concatenates them as strings lol
-                            const formatEndIndex = startIndex + 2; // this literally has to be calculated in separate, otherwise javascript concatenates them as strings lol
-
-                            setTimeout(() => {
-                                quill.formatText(formatStartIndex, formatEndIndex, 'code', true);
-                            }, 10);
-
-                            // After the format has been applied safely we delete the initial
-                            // backtick
-                            setTimeout(() => {
-                                quill.deleteText(startIndex, formatStartIndex);
-                            }, 20);
-                        }
-                        consecutiveBackticks = 0; // Reset the counter
-                    } else {
-
-                        // If any other key is pressed without meeting the conditions above, reset the counter
-                        consecutiveBackticks = 0;
-                    }
+                if (event.key !== 'I' && event.key !== 'Meta' && !event.shiftKey && !event.ctrlKey && !event.altKey) {
+                    // Reset presses
+                    console.log("YIKEE: "+ event.key);
+                    consecutivePresses = 0;
                 }
             };
 
