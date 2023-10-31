135d019178fb37f59d6d6be2aec2b2011a63d340
Tamas Szucs <tomfurrier@gmail.com>
2023-10-31 11:56:26 +0100

    track distinct_id

--- vsce/src/utils/mixpanelHelper.ts
+++ vsce/src/utils/mixpanelHelper.ts
@@ -1,23 +1,42 @@
-// MixpanelHelper.ts
 import * as mixpanel from 'mixpanel';
+import * as vscode from 'vscode';
+import { v4 as uuidv4 } from 'uuid';
 import { CONFIG } from '../config';
 
 class MixpanelHelper {
     private static instance: MixpanelHelper;
     private mixpanelInstance: mixpanel.Mixpanel;
+    private userId: string;
 
     private constructor(token: string, config?: mixpanel.InitConfig) {
         this.mixpanelInstance = mixpanel.init(token, config);
+
+        // Retrieve or generate the userId
+        this.userId = this.getUserId();
     }
 
     static getInstance(config?: mixpanel.InitConfig): MixpanelHelper {
         if (!MixpanelHelper.instance) {
-            MixpanelHelper.instance = new MixpanelHelper(CONFIG.mixpanelToken, config); // Use the token from the config file
+            MixpanelHelper.instance = new MixpanelHelper(CONFIG.mixpanelToken, config);
         }
         return MixpanelHelper.instance;
     }
 
+    private getUserId(): string {
+        const userId = vscode.workspace.getConfiguration().get<string>('yourExtensionName.userId');
+        if (userId) {
+            return userId;
+        }
+
+        const newUserId = uuidv4();
+        vscode.workspace.getConfiguration().update('yourExtensionName.userId', newUserId, vscode.ConfigurationTarget.Global);
+        return newUserId;
+    }
+
     trackEvent(eventName: string, properties: mixpanel.PropertyDict = {}): void {
+        // Add the userId to the properties
+        properties.distinct_id = this.userId;
+
         this.mixpanelInstance.track(eventName, properties, (err) => {
             if (err) {
                 console.error('Error sending event to Mixpanel:', err);
